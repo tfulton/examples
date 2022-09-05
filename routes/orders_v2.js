4a3a77d92d357fd4b823e536e51a0569c062499b
Todd Fulton <todd.s.fulton@gmail.com>
2022-09-05 13:11:12 -0700

    Updating straight PP checkout and moving into index.html.

--- routes/orders_v2.js
+++ routes/orders_v2.js
@@ -7,8 +7,6 @@ const buildAccessHeader = require('./authUtils').buildAccessHeader;
 const baseURL = config.get("env.sandbox.rest.baseURL");
 const ResponseInfo = require("./dataUtils").ResponseInfo;
 
-
-
 // do some initial prep
 router.use(function(req, res, next){
     console.log("Handling URL: " + req.originalUrl + ' ' + req.method);
@@ -78,30 +76,6 @@ function doSave(req, res, next){
 
 };
 
-router.post('/:orderId/save', doSave, function(req, res, next){
-
-    try {
-        const orderId = req.params.orderId;
-        const url = `${baseURL}/v2/checkout/orders/${orderId}/save`;
-        fetch(url, {
-            method: 'POST',
-            headers: req.ppHeader,
-            body: '{}'
-        }).then(function (response) {
-            return response.json();
-        }).then(function (json) {
-            console.log("Fetch json: ", json);
-            res.status(201).send(json);
-        }).catch(function (error) {
-            throw error;
-        });
-    }
-    catch (error) {
-        console.log("ERROR: ", error);
-        res.status(500).send("ERROR: ", JSON.stringify(error));
-    }
-});
-
 router.post('/:orderId/authorize', (req, res, next) => {
     try {
         var orderId = req.params.orderId;
@@ -133,11 +107,10 @@ router.post('/:orderId/capture', function(req, res, next){
         const orderId = req.params.orderId;
         console.log("OrderID for capture:  ", orderId);
         
+        console.log("BODY: ", JSON.stringify(req.body, null, 4));
         var body = req.body;
-        console.log("BODY: ", req.body);
         
         const url = `${baseURL}/v2/checkout/orders/${orderId}/capture`; 
-        console.log('URL: ', url);
         fetch(url, {
             method: 'POST',
             headers: req.ppHeader,
@@ -145,16 +118,41 @@ router.post('/:orderId/capture', function(req, res, next){
         }).then((response) => {
             return response.json();
         }).then((json) => {
-            console.log("order capture response json: ", JSON.stringify(json, null, 4));
+            console.log("order capture response json: ", json);
             res.setHeader('Content-Type', 'application/json');
             res.status(201).send(json);
         }).catch((error) => {
+            console.error('Error in response: ', error);
             res.status(400).send(JSON.stringify(new Error('Message rejected by endpoint.', error)));
         });
     }
     catch (error) {
         console.log("ERROR: ", error);
         res.status(500).send(JSON.stringify(error));
+    }
+});
+
+router.post('/:orderId/save', function(req, res, next){
+
+    try {
+        const orderId = req.params.orderId;
+        const url = `${baseURL}/v2/checkout/orders/${orderId}/save`;
+        fetch(url, {
+            method: 'POST',
+            headers: req.ppHeader,
+            body: '{}'
+        }).then(function (response) {
+            return response.json();
+        }).then(function (json) {
+            console.log("Fetch json: ", json);
+            res.status(201).send(json);
+        }).catch(function (error) {
+            throw error;
+        });
+    }
+    catch (error) {
+        console.log("ERROR: ", error);
+        res.status(500).send("ERROR: ", JSON.stringify(error));
     }
 });
 
