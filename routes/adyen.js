149f5d34b716118c7287c51983c6127d299a1b19
Todd Fulton <todd.s.fulton@gmail.com>
2022-09-18 12:53:06 -0700

    Auth+Capture working with Adyen example.

--- routes/adyen.js
+++ routes/adyen.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const config = require('config');
+const fetch = require("node-fetch");
 
 
 // do some initial prep
@@ -51,6 +52,41 @@ router.post('/payment', (req, res) => {
     console.log('BODY: ', JSON.stringify(body, null, 4));
 
     const url = baseURL + '/v68/payments';
+
+    (async () => {
+        const response = await fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "x-API-key": creds.apiKey
+            },
+            body: JSON.stringify(body)
+        });
+        const json = await response.json();
+
+        // // handle error response
+        // if (response.status !== 200) {
+        //     res.status(400).send(JSON.stringify(new Error('Message rejected by endpoint.')));
+        // }
+
+        // return non-error response
+        console.log("Endpoint response json: ", json);
+        res.setHeader('Content-Type', 'application/json');
+        res.status(201).send(json);
+    })();
+
+});
+
+// receive a payload and send on to the adyen payment endpoint
+router.post('/payment/:pspReference/capture', (req, res) => {
+
+    const body = req.body;
+    const pspReference = req.params.pspReference
+    console.log('BODY: ', JSON.stringify(body, null, 4));
+    console.log(`PSP Reference: ${pspReference}`);
+
+    const url = baseURL + `/v68/payments/${pspReference}/captures`;
+    console.log('URL for capture request: ', url);
 
     (async () => {
         const response = await fetch(url, {
